bf1d28848d96ba673ccdb5b81d379a4b477cd3e7
unknown <gigawave07@gmail.com>
2020-08-07 10:23:01 +0700

    up

---
+++ module 2/ke_thua/src/bai_tap/Cylinder.java
@@ -0,0 +1,26 @@
+package bai_tap;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(){}
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getVolume(){
+        return this.getArea()*this.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "height=" + height +
+                ", radius=" + getRadius() +
+                ", color=" + getColor() +
+                '}';
+    }
+}

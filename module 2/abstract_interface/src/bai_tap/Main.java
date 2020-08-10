e7fad29765fce5248ac8e35059ae5fcae2ffa18c
unknown <gigawave07@gmail.com>
2020-08-10 16:56:48 +0700

    up

--- module 2/abstract_interface/src/bai_tap/Main.java
+++ module 2/abstract_interface/src/bai_tap/Main.java
@@ -3,10 +3,10 @@ package bai_tap;
 public class Main {
 
     public static void main(String[] args) {
-        Circle circle = new Circle(10);
-        Square square = new Square(10);
-        Rectangle rectangle = new Rectangle(10, 20);
-        Shape[] array = {circle, square, rectangle};
+        Shape[] array = new Shape[3];
+        array[0] = new Circle(10);
+        array[1] = new Rectangle(10, 20);
+        array[2] = new Square(10);
 
         for (Shape shape : array) {
             System.out.println(shape);

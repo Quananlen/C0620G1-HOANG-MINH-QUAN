b2da0682e6931fd37d29d799077938262c1f9329
unknown <gigawave07@gmail.com>
2020-08-14 17:28:16 +0700

    up

--- module 2/queue_stack/src/bai_tap/CheckBracket.java
+++ module 2/queue_stack/src/bai_tap/CheckBracket.java
@@ -5,9 +5,21 @@ import java.util.Stack;
 public class CheckBracket {
     public static void main(String[] args) {
         String str = "(– b + (b^2 – 4*a*c)^(0.5/ 2*a))";
-        Stack stack = new Stack();
+        System.out.println(checkBracket(str));
+    }
+    public static boolean checkBracket(String str) {
+        Stack<Character> bStack = new Stack();
         for (int i = 0; i < str.length(); i++) {
-
+            boolean isOpenBracket = Character.compare(str.charAt(i), '(') == 0;
+            boolean isCloseBracket = Character.compare(str.charAt(i), ')') == 0;
+            if (isOpenBracket) bStack.push('(');
+            if (isCloseBracket) {
+                if (bStack.isEmpty()) {
+                    return false;
+                }
+                bStack.pop();
+            }
         }
+        return bStack.isEmpty();
     }
 }

53d78e5137d91ac19ca5d9e672cc950a763b0614
unknown <gigawave07@gmail.com>
2020-08-14 15:55:53 +0700

    up

--- module 2/queue_stack/src/bai_tap/TreeMapTest.java
+++ module 2/queue_stack/src/bai_tap/TreeMapTest.java
@@ -5,20 +5,18 @@ import java.util.TreeMap;
 
 public class TreeMapTest {
     public static void main(String[] args) {
-        TreeMap<Character, Integer> treeMap = new TreeMap();
-        String str = "This is a tree map aaa";
+        TreeMap<Character, Integer> treeMap = new TreeMap<>();
+        String str = "abbcccddddeeeee";
         str = str.toUpperCase();
-        for(int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (!treeMap.containsKey(c)) {
-                treeMap.put(c, 1);
-            } else {
+            if (!treeMap.containsKey(c)) treeMap.put(c, 1);
+            else {
                 for (Map.Entry<Character, Integer> entry : treeMap.entrySet()) {
-                    int value = entry.getValue();
                     if (entry.getKey().equals(c)) {
+                        int value = entry.getValue();
                         treeMap.put(c, ++value);
                     }
-                    break;
                 }
             }
         }
